7f6d2a0cf1fbcd65219a0615dabca2fb25f39bf4
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-12-10 10:06:23 +0700

    artificial rollback

--- app/src/main/java/com/example/md_lab08__retrofit_multiple_viewholder/MainActivity.kt
+++ app/src/main/java/com/example/md_lab08__retrofit_multiple_viewholder/MainActivity.kt
@@ -5,11 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,29 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
-        val rView: RecyclerView = findViewById(R.id.rView)
-        val adapter: Adapter = Adapter(DiffCallback())
-
-        setSupportActionBar(toolbar)
-        rView.adapter = adapter
-        rView.layoutManager = LinearLayoutManager(this)
-
-        val mService: RetrofitServices = Common.retrofitService
-        val lat = 54.2021736
-        val lon = 30.2964015
-        val appId = "enter your api key"
-
-        mService.getWeatherList(lat, lon, appId).enqueue(object : Callback<WeatherWrapper> {
-            override fun onResponse(call: Call<WeatherWrapper>, response: Response<WeatherWrapper>) {
-                if (response.isSuccessful) {
-                    val forecast = response.body()
-                    adapter.submitList(forecast!!.list)
-                }
-            }
-
-            override fun onFailure(call: Call<WeatherWrapper>, t: Throwable) { }
-        })
     }
 }
